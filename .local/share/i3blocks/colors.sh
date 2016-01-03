b695e1d7510c6a9b44983dcbafb7049f53e1edb7
Tom Jakubowski <tom@crystae.net>
2016-01-02 21:12:09 -0600

    i3blocks: Use custom script for wifi
    also tweak i3bar background color so it doesn't blend in with
    termite, which uses base00

---
+++ .local/share/i3blocks/colors.sh
@@ -0,0 +1,23 @@
+# Source me for base16-eighties colors.
+
+base00="#2d2d2d"
+base01="#393939"
+base02="#515151"
+base03="#747369"
+base04="#a09f93"
+base05="#d3d0c8"
+base06="#e8e6df"
+base07="#f2f0ec"
+base08="#f2777a"
+base09="#f99157"
+base0a="#ffcc66"
+base0b="#99cc99"
+base0c="#66cccc"
+base0d="#6699cc"
+base0e="#cc99cc"
+base0f="#d27b53"
+
+COLOR_RED=$base08
+COLOR_ORANGE=$base09
+COLOR_YELLOW=$base0a
+COLOR_GREEN=$base0b
