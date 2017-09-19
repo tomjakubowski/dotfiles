638989e2c9d77807c37f291b389546a2e39bba60
Tom Jakubowski <tom@crystae.net>
2017-09-19 00:09:56 -0700

    xsession: quieter

--- x/.xsession.zsh
+++ x/.xsession.zsh
@@ -1,14 +1,12 @@
 #!/bin/zsh
-#
+
 function maybe_source() {
     [[ -r $1 ]] && . $1
 }
-
-echo "oh hai" > ~/diditwork
 
 maybe_source ~/.zshenv
 maybe_source ~/.zshrc
 maybe_source ~/.zlogin
 
-command hsetroot && hsetroot -solid '#F5F5DC' -center "$HOME/.local/share/wallpaper"
+command hsetroot 2>/dev/null && hsetroot -solid '#F5F5DC' -center "$HOME/.local/share/wallpaper"
 exec i3
