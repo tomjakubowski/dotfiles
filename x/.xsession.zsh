aab21d26ba1707ec5ed9ea99c84428969c5b5bfb
Tom Jakubowski <tom@crystae.net>
2018-01-11 16:31:37 -0800

    add temporary countermeasure for skullcanyon kbd

--- x/.xsession.zsh
+++ x/.xsession.zsh
@@ -1,5 +1,7 @@
 #!/bin/zsh
 
+set -e
+
 function maybe_source() {
     [[ -r $1 ]] && . $1
 }
@@ -9,4 +11,7 @@ maybe_source ~/.zshrc
 maybe_source ~/.zlogin
 
 command hsetroot 2>/dev/null && hsetroot -solid '#F5F5DC' -center "$HOME/.local/share/wallpaper"
+if [[ "$HOST" == "tjakubowski-sc" ]]; then
+    setxkbmap -model pc105 -layout us,us -option ctrl:nocaps
+fi
 exec i3
