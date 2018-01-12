8a9dbc3a70e29a8de3bfe9397dd74d1fadee5a96
Tom Jakubowski <tjakubowski@oblong.com>
2018-01-11 16:30:24 -0800

    add git-oblong config
    sets git env vars

---
+++ git-oblong/.config/zshenv.d/git-env.zsh
@@ -0,0 +1,8 @@
+export GIT_AUTHOR_NAME="Tom Jakubowski"
+export GIT_AUTHOR_EMAIL="tjakubowski@oblong.com"
+export GIT_COMMITTER_NAME="$GIT_AUTHOR_NAME"
+export GIT_COMMITTER_EMAIL="$GIT_AUTHOR_EMAIL"
+
+# Local Variables:
+# sh-shell: "zsh"
+# End:
