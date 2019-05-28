c97156f164b90a6236bd97f5375bea3e711c8dba
Tom Jakubowski <tom@crystae.net>
2019-05-28 16:27:11 -0700

    ruby: set RUBIES after sourcing chruby

--- ruby/.config/zshrc.d/ruby.zsh
+++ ruby/.config/zshrc.d/ruby.zsh
@@ -1,12 +1,13 @@
 init_chruby() {
   setopt local_options nonomatch
-  RUBIES+=($HOME/opt/rubies/*)
-
-  # Homebrew
-
+  # Homebrew location:
   if [[ -f /usr/local/share/chruby/chruby.sh ]]; then
     source /usr/local/share/chruby/chruby.sh
+  else
+    echo "ruby dotfiles: can't find chruby to source"
+    return 1
   fi
+  RUBIES+=($HOME/opt/rubies/*)
 }
 
 init_chruby
