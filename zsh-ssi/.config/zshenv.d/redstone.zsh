15f45b701164c3c4f1c6db45b4b3e8dc161e8214
Tom Jakubowski <tom@secondspectrum.com>
2022-05-29 14:54:53 -0700

    No more asdf, wrangle paths by hand instead

---
+++ zsh-ssi/.config/zshenv.d/redstone.zsh
@@ -0,0 +1,6 @@
+path=(
+  /opt/homebrew/opt/erlang@23/bin
+  ~/elixir/v1.11.4/bin
+  ~/nodejs/node-v16.15.0-darwin-arm64/bin
+  ${path[@]}
+)
--- zsh-ssi/.config/zshrc.d/ssi.zsh
+++ zsh-ssi/.config/zshrc.d/ssi.zsh
@@ -1,3 +1,4 @@
 path+=(~/.ssi/bin)
+path+=(/Applications/Postgres.app/Contents/Versions/14/bin)
 
 alias ssi-drip-toolkit='ssi-drip-toolkit --env=prod --dir=.'
