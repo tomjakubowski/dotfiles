dd0392d6b0ff5dd1564aabd443d36dface008153
Tom Jakubowski <tom@secondspectrum.com>
2022-05-29 14:55:07 -0700

    Fix init.sh script

--- init.sh
+++ init.sh
@@ -1,6 +1,8 @@
-#!/bin/sh
+#!/bin/zsh
 
 set -ex
 
-mkdir -p ~/.config/{zshenv.d, zshrc.d}
-mkdir -p ~/.local/{bin, share}
+mkdir -p ~/.config/zshenv.d
+mkdir -p ~/.config/zshrc.d
+mkdir -p ~/.local/bin
+mkdir -p ~/.local/share
