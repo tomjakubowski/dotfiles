268c13f5301a85386c4adbf0ab07087ec08031c3
Tom Jakubowski <tom@crystae.net>
2022-08-25 22:55:15 -0700

    treesitter: heex

--- nvim/.config/nvim/lua/treesitter.lua
+++ nvim/.config/nvim/lua/treesitter.lua
@@ -1,5 +1,5 @@
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = {"elixir"},
+  ensure_installed = {"elixir", "heex"},
   sync_install = false,
   auto_install = true,
   highlight = {
